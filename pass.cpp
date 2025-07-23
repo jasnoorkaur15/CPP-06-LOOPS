57f9dc5c482bfe86b5660d87cd4ff255ca22000e
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-07-23 15:30:18 +0530

    Create pass.cpp

---
+++ pass.cpp
@@ -0,0 +1,19 @@
+//Jasnoor Kaur
+//24070123049
+#include<iostream>
+using namespace std;
+#include<string>
+
+int main(){
+    string password = "mannat";
+    string user_pass;
+
+    cout<<"code batao";
+    cin>>user_pass;
+    if (user_pass==password)
+    cout<<"unlocked";
+    else cout<<"bas karo, nahi hoga";
+   
+return 0;
+
+}
