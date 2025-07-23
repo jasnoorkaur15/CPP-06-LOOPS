4d99596ed65fc05a9b533e20b41bb5b85aa36245
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-07-23 14:59:34 +0530

    Create even values.cpp

---
+++ even values.cpp
@@ -0,0 +1,15 @@
+//Jasnoor Kaur
+//24070123049
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+   
+       for (int i=0;i<11;i++){
+        if (i%2==0)
+    cout <<i<<"\n";
+    }
+    return 0;
+
+}
